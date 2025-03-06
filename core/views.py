75baab957440409d80ec0ce46f18b9593cb36fc2
Matheus <matheus.brgsz@gmail.com>
2025-03-06 10:14:41 -0300

    add home page template and head partial; update view and URL routing

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'core/home.html', context={"name": "matheus"})
+    return render(request, 'core/pages/home.html', context={"name": "matheus"})
 
