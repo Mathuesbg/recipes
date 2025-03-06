791ab2ac5f22febc81e9cd2dd992e5d7da1d7de2
Matheus <matheus.brgsz@gmail.com>
2025-03-06 09:13:38 -0300

    remove unused base view from core views

--- core/views.py
+++ core/views.py
@@ -4,5 +4,3 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'core/home.html', context={"name": "matheus"})
 
-def base(request):
-    return render(request, 'global/home.html')
