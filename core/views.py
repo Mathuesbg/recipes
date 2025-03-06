e097ff244d05aacdece7cb008b6ab9d543eb4903
Matheus <matheus.brgsz@gmail.com>
2025-03-06 09:03:55 -0300

    add core app with home and base views, templates, and URL routing

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'core/home.html', context={"name": "matheus"})
+
+def base(request):
+    return render(request, 'global/home.html')
