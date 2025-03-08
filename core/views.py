0be1fa6887696419c66ff3d61d441ff67ed3f1f1
Matheus <matheus.brgsz@gmail.com>
2025-03-08 15:10:04 -0300

    refactor templates to extend global base and improve recipe display; add recipe factory for test data

--- core/views.py
+++ core/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
-    return render(request, 'core/pages/home.html', context={"name": "matheus"})
+    return render(
+            request=request, 
+            template_name='core/pages/home.html', 
+            context={
+            "recipes": [make_recipe() for _ in range(10)],
+            }
+        )
 
 def recipe(request, id):
-        return render(request, 'core/pages/recipe-view.html', context={"name": "matheus"})
+        return render(
+            request=request,
+            template_name='core/pages/recipe-view.html',
+            context={
+                "recipe": make_recipe(),
+                "is_detail_page" : True
+                }
+            )
