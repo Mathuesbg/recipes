48ac87fd0eb626aa4ac46533ca5c11f6d5816ae3
Matheus <matheus.brgsz@gmail.com>
2025-03-09 17:48:43 -0300

    add author field to Recipe model, update migrations, and enhance admin interface; implement category view and update templates for improved recipe display

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core import models
 
-# Register your models here.
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
--- core/views.py
+++ core/views.py
@@ -1,22 +1,49 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from core import models
 
 # Create your views here.
 def home(request):
+    recipes = models.Recipe.objects.filter(is_published=True).order_by("-id")
     return render(
             request=request, 
             template_name='core/pages/home.html', 
             context={
-            "recipes": [make_recipe() for _ in range(10)],
+            "recipes": recipes,
             }
         )
 
 def recipe(request, id):
+        recipe = get_object_or_404(
+            models.Recipe.objects.filter(
+                pk=id, 
+                is_published=True
+                )
+            )
+
         return render(
+             
             request=request,
             template_name='core/pages/recipe-view.html',
             context={
-                "recipe": make_recipe(),
+                "recipe": recipe,
                 "is_detail_page" : True
                 }
             )
+
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+        models.Recipe.objects.filter(
+            category__id = category_id, 
+            is_published=True,
+        ).order_by("-id")
+    )
+    
+    return render(  
+            request=request, 
+            template_name='core/pages/category.html', 
+            context={
+            "recipes": recipes,
+            "title": f"{recipes[0]} - Category",
+            }
+        )
