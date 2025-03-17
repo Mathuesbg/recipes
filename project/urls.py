a7bc758958dadc562e4df276f79c284d46fef5b1
Matheus <matheus.brgsz@gmail.com>
2025-03-17 09:43:23 -0300

    add authors app; create models, views, templates, and URLs for author registration

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('authors/', include('authors.urls')),
 ]
 
 urlpatterns += static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
