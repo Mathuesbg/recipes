ac7c300b106712a7646955e2739ab168525ee0a4
Matheus <matheus.brgsz@gmail.com>
2025-03-25 18:00:55 -0300

    add logout functionality; implement login view with authentication and error handling; enhance login template with user feedback

--- authors/views.py
+++ authors/views.py
@@ -3,6 +3,9 @@ from django.urls import reverse
 from authors.forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.decorators import login_required
+
 
 def register_author(request):
     
@@ -32,6 +35,7 @@ def register_create(request):
         user = form.save()
         messages.success(request, 'Author registered successfully!')
         del(request.session['register_form_data'])
+        return redirect("authors:login")
 
     return redirect('authors:register')
 
@@ -48,7 +52,53 @@ def login_view(request):
     )
 
 def login_create(request):
-    return render(
+    
+    if not request.POST:
+        raise Http404  
+    
+    form = LoginForm(request.POST)
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+            )
+        
+        if authenticated_user is not None:
+
+            login(request=request, user=authenticated_user)
+
+            messages.success(
+                request=request, 
+                message="User Logged in!"
+                )
+            
+            return redirect("recipe:home")
+        
+        messages.error(
+            request=request,
+            message="invalid credentials"
+            )
+        return redirect("authors:login")
+    
+    messages.error(
         request=request,
-        template_name= "authors/pages/login.html",
-    )
+        message="Form error!"
+        )
+    
+    return redirect("authors:login")
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+    
+    if request.POST.get("username") != request.user.username:
+        return redirect(reverse('authors:login'))
+
+    post = request.POST
+    user = request.user
+
+    logout(request=request)
+
+    return redirect(reverse('authors:login'))
