1f6195c5463e860ba8f377ef0ba94aa28ce0b81f
Matheus <matheus.brgsz@gmail.com>
2025-04-02 13:15:57 -0300

    add validation to AuthorRecipeForm; ensure title and description are not equal, enforce minimum title length, and validate positive preparation time

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,13 +1,18 @@
+from collections import defaultdict
+
 from django import forms
 from core.models import Recipe
 from utils.django_forms import add_attr
+from utils.strings import is_positive_number
 
 
 class AuthorRecipeForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        add_attr(self.fields.get('cover'), 'class', "span-2")
+
+        self._my_errors = defaultdict(list)
+
         add_attr(self.fields.get('preparation_steps'), 'class', "span-2")
 
     class Meta:
@@ -18,6 +23,7 @@ class AuthorRecipeForm(forms.ModelForm):
         'preparation_steps', 'cover', 'category')
 
         widgets = {
+
             'cover' : forms.FileInput(
                 attrs={
                 'class' : 'span-2'
@@ -37,5 +43,45 @@ class AuthorRecipeForm(forms.ModelForm):
                     ('Horas','Horas'),
                 ),
             ),
-
         }
+
+
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        
+        cleaned_data = self.cleaned_data
+        title = cleaned_data.get("title")
+        description = cleaned_data.get("description")
+
+        if description == title:
+            self._my_errors['title'].append(
+                "Title can not be equal to description"
+                )
+            self._my_errors['description'].append(
+                "Description can not be equal to title"
+                )
+
+        if self._my_errors:
+            raise forms.ValidationError(self._my_errors)
+
+        return super_clean
+    
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+
+        if len(title) < 5:
+            self._my_errors['title'].append(
+                "Title must have at least 5 characters"
+                )
+
+        return title
+    
+    def clean_preparation_time(self):
+        preparation_time = self.cleaned_data.get("preparation_time")
+
+        if not is_positive_number(preparation_time):
+            self._my_errors['preparation_time'].append(
+                "Preparation time must be greatte than 0"
+                )
+        
+        return preparation_time 
